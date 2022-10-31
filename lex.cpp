27073a5686c232b900f0721f28cd7b4650d13684
cdwilkie <80400795+cdwilkie@users.noreply.github.com>
2022-10-30 21:19:45 -0400

    lex.cpp added

---
+++ lex.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    if (argc > 1 ) {
+        
+    cout << argv[1] << endl;
+    }
+    return 0;
+}
