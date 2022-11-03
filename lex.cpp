52f072dc7b0a5181bf6b536065700c5cac583288
cdwilkie <80400795+cdwilkie@users.noreply.github.com>
2022-11-03 14:04:54 -0400

    Testing
    "hello, world!!"

--- lex.cpp
+++ lex.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv) {
     if (argc > 1 ) {
         
     cout << argv[1] << endl;
-    cout << "Hello, World!" << endl;
+    cout << "Hello, World!!" << endl;
     }
     return 0;
 }
