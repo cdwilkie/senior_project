b6d0845faf4ef8c31b51fd6e162a6bd7e1b8579c
cdwilkie <cdwilkie@aggies.ncat.edu>
2023-03-28 00:39:59 -0400

    Added CMake build files
    Added CMake build files and executable 'highlight'

---
+++ Highlight/build/CMakeFiles/highlight.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/dustinwilkie/Documents/GitHub/senior_project/highlight/src/FileReader.cpp" "CMakeFiles/highlight.dir/src/FileReader.cpp.o" "gcc" "CMakeFiles/highlight.dir/src/FileReader.cpp.o.d"
+  "/Users/dustinwilkie/Documents/GitHub/senior_project/highlight/src/main.cpp" "CMakeFiles/highlight.dir/src/main.cpp.o" "gcc" "CMakeFiles/highlight.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Highlight/build/CMakeFiles/highlight.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/highlight.dir/src/FileReader.cpp.o"
+  "CMakeFiles/highlight.dir/src/FileReader.cpp.o.d"
+  "CMakeFiles/highlight.dir/src/main.cpp.o"
+  "CMakeFiles/highlight.dir/src/main.cpp.o.d"
+  "highlight"
+  "highlight.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/highlight.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
