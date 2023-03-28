374cd5ddd88c8cdbcc04d46702c6379ca4d3227a
cdwilkie <cdwilkie@aggies.ncat.edu>
2023-03-28 02:32:03 -0400

    Command-Line Arguments
    Updated main to support command line arguments. Main now passes first argument entered as filename. If no arguments entered, main prompts for filename by calling UserInput::getFilename()

--- Highlight/src/main.cpp
+++ Highlight/src/main.cpp
@@ -2,9 +2,15 @@
 #include "../include/UserInput.h"
 #include <iostream>
 
-int main () {
-    std::string filename = UserInput::getFilename();
-    auto lines = FileReader::readLines(filename);
+int main (int argc, char** argv) {
+    if (argc <= 1) {
+        std::string filename = UserInput::getFilename();
+        auto lines = FileReader::readLines(filename);
+    }
+    else {
+        auto lines = FileReader::readLines(argv[1]);
+    }
+    
 
 
     return 0;
