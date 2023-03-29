a31ee44807d73b7f2e621b05fc3504ddff73a489
cdwilkie <cdwilkie@aggies.ncat.edu>
2023-03-29 06:09:43 -0400

    Updated main.cpp
    Added comments for clarity. Removed test for Tokenize.

--- Highlight/src/main.cpp
+++ Highlight/src/main.cpp
@@ -1,38 +1,29 @@
 #include "../include/FileHandler.h"
 #include "../include/UserInput.h"
 #include "../include/Tokenizer.h"
-#include <iostream>
+
 
 int main (int argc, char** argv) {
 
-    std::vector<std::wstring> lines;
+    std::vector<std::wstring> lines;            //Holds file contents, one line per index
     
-    if (argc <= 1) {
-        std::string inFile = UserInput::getFilename();
-        lines = FileHandler::readLines(inFile);
-    }
-    else {
-        lines = FileHandler::readLines(argv[1]);
-    }
+    if (argc <= 1) {                            //If no arguments given
+        std::string inFile = UserInput::getFilename();//Prompt user for filename
+        lines = FileHandler::readLines(inFile); //Open file and read lines
+    }//end if no argument
+    else {                                      //Else argument given 
+        lines = FileHandler::readLines(argv[1]);//Use argument as filename, open, readlines
+    }//end if argument
     
-    std::string outFile = UserInput::getFilename();
-    FileHandler::writeLines(outFile, lines);
+    std::string outFile = UserInput::getFilename();//Prompt for outfile location
+    FileHandler::writeLines(outFile, lines);    //Write lines to outfile
 
     //Test tokenizer
-    std::vector<std::vector<std::wstring>> tokens;
-    for (int i = 0; i < lines.size(); ++i) {
-        tokens.push_back(Tokenizer::tokenize(lines[i]));
-        for (int j = 0; j < tokens[i].size(); ++j) {
-            if (! (j == tokens[i].size()-1)) {
-                std::wcout << tokens[i][j] << "  *  ";
-            }
-            else {
-                std::wcout << tokens[i][j];
-            }
-        }
-        std:: wcout << std::endl;
-        
-    }
+    std::vector<std::vector<std::wstring>> tokens;//2-D Holds tokens from each line
+
+    for (int i = 0; i < lines.size(); ++i) {    //Iterate through each line
+        tokens.push_back(Tokenizer::tokenize(lines[i]));//Tokenize each word and add to vector
+    }//end for 
 
     return 0;
 }
