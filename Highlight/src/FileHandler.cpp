431a788b3ea6388068a7a06526cf45ddbca034b4
cdwilkie <cdwilkie@aggies.ncat.edu>
2023-05-02 19:23:44 -0400

    Comments to FileHandler.cpp
    Added comments for FileHandler.cpp

--- Highlight/src/FileHandler.cpp
+++ Highlight/src/FileHandler.cpp
@@ -1,6 +1,8 @@
 /*
-* FileHandler class supports reading UTF-8 encoded
-* files. 
+* FileHandler class supports wide char (Unicode) file
+* functionality for reading in Unicode-containing files
+* and writing to html the contents text source. Functionality
+* to build key-value maps from read files.
 */
 #include "../include/FileHandler.h"
 #include <fstream>
@@ -30,73 +32,90 @@ std::vector<std::wstring> FileHandler::readLines(const std::string& filename) {
     }//end while lines
     file.close();
     return lines;                               //Return the populated vector
-}
+}//end readLines
 
+/* readKeyValues() accepts a string file pathname 
+* and opens a file stream with that location. Reads each
+* line and parses the line to extract a key and value,
+* where each line of the source is expected to have a key
+* enclosed in quotes and a value enclosed in quotes, i.e.
+* ("key" "value"). Keys with no known value should be
+* associated as "null" in the .txt source. Returns unordered_map
+*
+*/
 std::unordered_map<std::wstring, std::wstring> FileHandler::readKeyValues(
         const std::string& filename) {
-    std::unordered_map<std::wstring, std::wstring> keyValues;
-    std::wifstream file(filename);
+    std::unordered_map<std::wstring, std::wstring> keyValues;//Holds parsed data
+    std::wifstream file(filename);              //Opens unicode compatible file stream
 
-    if (!file || !file.is_open()) {
-        throw std::runtime_error("Failed to open file " + filename);
-        return keyValues;
-    }
-    std::wstring line;
-    while (std::getline(file, line)) {
-        std::wistringstream wiss(line);
-        std::wstring key, value;
+    if (!file || !file.is_open()) {             //If error opening file
+        throw std::runtime_error("Failed to open file " + filename);//throw error
+        return keyValues;                       //return map and exit func
+    }//end if error opening
+    std::wstring line;                          //Contents of current line
+    while (std::getline(file, line)) {          //While lines in source
+        std::wistringstream wiss(line);         //Unicode compatible string stream
+        std::wstring key, value;                //Will hold extracted key and value
         if (std::getline(wiss, key, L'"') && std::getline(wiss, key, L'"') &&
             std::getline(wiss, value, L'"') && std::getline(wiss, value, L'"')){
-            keyValues[key] = value;
-        }
-    }
-    file.close();
-    return keyValues;
-}
+            keyValues[key] = value;             //If correct format, extract and add key/value
+        }//end if correct format
+    }//end while lines
+    file.close();                               //Close file stream
+    return keyValues;                           //Return map
+}//end readKeyValues
 
+/*writeToHtml() opens a wide file stream, specified by the passed
+* parameter and iterates through the passed vector to write its
+* contents to file, encoded in HTML.
+*
+*/
 void FileHandler::writeToHtml(const std::string& filename,
         const std::vector<std::vector<Tokenizer::Token>>& tokens) {
 
-    std::wofstream file(filename);
-    if (!file.is_open() || !file) {
-        throw std::runtime_error("Faild to open file " + filename);
-    }
+    std::wofstream file(filename);              //Opens unicode compatible file stream
+    if (!file.is_open() || !file) {             //If error opening file
+        throw std::runtime_error("Faild to open file " + filename);//throw error
+    }//end if error opening file
 
-    file << "<!DOCTYPE html>\n<html>\n<head>\n"
+    file << "<!DOCTYPE html>\n<html>\n<head>\n" //Write HTML header contents to file
         << "<meta charset=\"UTF-8\">\n<title>Output</title>\n</head>\n<body>\n";
 
-    for (int i = 0; i < tokens.size(); ++i) {
-        for (int j = 0; j < tokens[i].size(); ++j) {
+    for (int i = 0; i < tokens.size(); ++i) {   //Iterate through vector
+        for (int j = 0; j < tokens[i].size(); ++j) {//Iterate through every token
  
-            if (tokens[i][j].tokenID == 200) {
-                file << "<span style=\"font-weight:bold;\">";
-                file << tokens[i][j].word << L" ";
-                file << "</span>";
-            }
+            if (tokens[i][j].tokenID == 200) {  //If token matches (id = 200)
+                file << "<span style=\"font-weight:bold;\">";//Bold the contents
+                file << tokens[i][j].word << L" ";//Token contents with unicode space
+                file << "</span>";              //Close the bold contents
+            }//end if token contents match
 
-            else {
-                file << "<span>";
-                file << tokens[i][j].word << L" ";
-                file << "</span>";
-            }
-        }
-        file << "<br>\n";
-    }
-    file << "</body>\n</html>";
-    file.close();
-    std::cout << "Lines written successfully to " << filename << std::endl;
+            else {                              //Else token is regular (no match)
+                file << "<span>";               //Enclose contents regular text
+                file << tokens[i][j].word << L" ";//Contents with unicode space
+                file << "</span>";              //Close regular contents
+            }//end else token regular not match
+        }//end for every token
+        file << "<br>\n";                       //Insert newline into HTML file
+    }//end for entire vector
+    file << "</body>\n</html>";                 //Close tags of HTML file
+    file.close();                               //Close file
+    std::cout << "Lines written successfully to " << filename << std::endl;//Confirmation
+}//end writeToHtml()
 
-}
-
+/*_escapeHtml() is a helper function for writeToHtml() that
+* escapes characters that could be interpreted as markup, etc.
+*
+*/
 std::wstring FileHandler::_escapeHtml(const std::wstring& word) {
-    std::wstring output;
-    output.reserve(word.size());
-    for (const auto& symbol : word) {
-        switch (symbol) {
-            case '<': 
-                output += L"&lt;";
-                break;
-        }
-    }
-    return output;
+    std::wstring output;                        //Holds escaped data
+    output.reserve(word.size());                //Resizes the container to size
+    for (const auto& symbol : word) {           //Iterate through each char in word
+        switch (symbol) {                       //Check different possible symbols
+            case '<':                           //Matches < symbol
+                output += L"&lt;";              //Escape symbol and concat to output
+                break;                          //exit switch
+        }//end switch symbol
+    }//end for every char
+    return output;                              //return escaped data
 }
