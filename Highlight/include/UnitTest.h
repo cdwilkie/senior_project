b419183ee7f885aa52e7089a13df782b88048dd1
cdwilkie <cdwilkie@aggies.ncat.edu>
2023-05-02 22:11:47 -0400

    Updated UnitTest.h
    Updated comments for UnitTest.h

--- Highlight/include/UnitTest.h
+++ Highlight/include/UnitTest.h
@@ -1,10 +1,27 @@
+/*UnitTest encapsulates timing functionality
+* in order to evaluate runtime performance
+* of the program in various phases of operation.
+* Supports a start, stop, and duration calcuation.
+*/
 #pragma once
 
 #include <chrono>
 
 class UnitTest {
 public:
+    /*start() establishes a time point to serve as the start time
+    *Returns time_point
+    */
     static std::chrono::time_point<std::chrono::high_resolution_clock> start();
+    
+    /*stop() establishes a time point to serve as the stop time
+    * Returns a time_point
+    */
     static std::chrono::time_point<std::chrono::high_resolution_clock> stop();
+    
+    /*duration() accepts a start and stop time and
+    * calculates the amount of time that transpired
+    * between the two time points
+    */
     static void duration(std::chrono::time_point<std::chrono::high_resolution_clock> start, std::chrono::time_point<std::chrono::high_resolution_clock> stop);
 };
